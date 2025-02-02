2d68f0f7b214a2db5f63782ff4d41b9cfd7498ea
abeni-al7 <abenezeralebachew3@gmail.com>
2025-02-02 15:18:50 +0300

    Add profile creation to the social-auth pipeline

--- account/authentication.py
+++ account/authentication.py
@@ -1,3 +1,4 @@
+from account.models import Profile
 from django.contrib.auth.models import User
 
 
@@ -20,3 +21,10 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+
+def create_profile(backend, user, *args, **kwargs):
+    """
+    Create user profile for social authentication
+    """
+    Profile.objects.get_or_create(user=user)
